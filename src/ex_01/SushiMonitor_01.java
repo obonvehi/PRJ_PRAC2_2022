720cdf4b00ff3a49b0d847fec480c563c551f3d8
jparpal <64012369+jparpal@users.noreply.github.com>
2022-03-19 13:34:49 +0100

    Prints acabat ex1

--- src/ex_01/SushiMonitor_01.java
+++ src/ex_01/SushiMonitor_01.java
@@ -8,8 +8,9 @@ public class SushiMonitor_01 {
 	private final static int MAX_CAPACITY = 5;
 	private boolean fullGroup = false;
 	private int numCustomers;
-	private int nfs;
-
+	private int nfs = 5;
+	private boolean first = true;
+	
 	private ReentrantLock lock = new ReentrantLock();
 	Condition noGroup = lock.newCondition();
 
@@ -17,11 +18,17 @@ public class SushiMonitor_01 {
 		/* COMPLETE */
 		lock.lock();
 		while (numCustomers == MAX_CAPACITY || fullGroup) {
-			if (nfs == 0) {
+			System.out.println("----> Entering "+"C("+i+")");
+			if (nfs == 0 && first) {
+				System.out.println(" *** Possible group detected. I wait "+"C("+i+")");
+				first = false;
 				fullGroup = true;
+			} else {
+				System.out.println(" *** I'm told to wait for all free "+"C("+i+")");
 			}
 			try {noGroup.await();} catch (InterruptedException e) {}
 		}
+		System.out.println(" +++ [free: " + nfs + "] I sit down C("+i+")");
 		numCustomers++;
 		nfs--;
 		lock.unlock();
@@ -32,6 +39,7 @@ public class SushiMonitor_01 {
 		lock.lock();
 		numCustomers--;
 		nfs++;
+		System.out.println("---> Now leaving [free: "+ nfs + "] "+"C("+i+")");
 
 		if(nfs==5)
 			fullGroup = false;
