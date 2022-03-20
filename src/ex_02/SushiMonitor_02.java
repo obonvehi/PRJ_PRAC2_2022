2ac3d458d763b71569cf0ffb5e5e2a7b515978dd
jparpal <64012369+jparpal@users.noreply.github.com>
2022-03-20 21:33:03 +0100

    ex2 correcte

--- src/ex_02/SushiMonitor_02.java
+++ src/ex_02/SushiMonitor_02.java
@@ -6,52 +6,55 @@ import java.net.*;
 import java.util.*;
 
 public class SushiMonitor_02 {
-	
 
 	/* COMPLETE */
 	private volatile boolean fullGroup = false;
 	private volatile int nfs = 5;
-	
+
 	private volatile int nextCustomer = 1;
 	private volatile int nowServing = 1;
 
 	private ReentrantLock lock = new ReentrantLock();
 	Condition noGroup = lock.newCondition();
-	
-	public void enter (int i) {
+
+	public void enter(int i) {
 		/* COMPLETE */
 		lock.lock();
 		int currentCustomer = nextCustomer;
 		nextCustomer++;
-		System.out.println("----> Entering "+"C("+i+")");
-		while (nfs==0 || fullGroup || currentCustomer!=nowServing) {
-			
-			if (!fullGroup&&nfs==0) {
-				System.out.println(" *** Possible group detected. I wait "+"C("+i+")");
+		System.out.println("----> Entering " + "C(" + i + ")");
+		while (nfs == 0 || fullGroup || currentCustomer != nowServing) {
+
+			if (!fullGroup && nfs == 0) {
+				System.out.println(" *** Possible group detected. I wait " + "C(" + i + ")");
 				fullGroup = true;
-			
-			} else if (fullGroup/*&&currentCustomer==nowServing*/) {System.out.println(" *** I'm told to wait for all free "+"C("+i+")");}
-			
-			if (!fullGroup) noGroup.signal();
-			
-			noGroup.awaitUninterruptibly(); 	
+
+			} else if (fullGroup/* &&currentCustomer==nowServing */) {
+				System.out.println(" *** I'm told to wait for all free " + "C(" + i + ")");
+			}
+
+			if (!fullGroup)
+				noGroup.signal();
+
+			noGroup.awaitUninterruptibly();
 		}
-		System.out.println(" +++ [free: " + nfs + "] I sit down C("+i+")");
+		System.out.println(" +++ [free: " + nfs + "] I sit down C(" + i + ")");
 		nfs--;
 		nowServing++;
 		lock.unlock();
-	
+
 	}
-	
-	public void exit (int i) {
+
+	public void exit(int i) {
 		/* COMPLETE */
 		lock.lock();
 		nfs++;
-		System.out.println("---> Now leaving [free: "+ nfs + "] "+"C("+i+")");
+		System.out.println("---> Now leaving [free: " + nfs + "] " + "C(" + i + ")");
 
-		if(nfs==5) fullGroup = false;
-			
-		noGroup.signal();
+		if (nfs == 5) {
+			fullGroup = false;
+			noGroup.signal();
+		}
 		lock.unlock();
 	}
 }
