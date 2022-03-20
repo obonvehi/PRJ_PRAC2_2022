f389960c15afc83c6e30db16666ef1175ed416d4
jparpal <64012369+jparpal@users.noreply.github.com>
2022-03-20 19:57:13 +0100

    ex1 simplificat

--- src/ex_01/SushiMonitor_01.java
+++ src/ex_01/SushiMonitor_01.java
@@ -5,11 +5,9 @@ import java.util.concurrent.locks.*;
 public class SushiMonitor_01 {
 
 	/* COMPLETE */
-	private final static int MAX_CAPACITY = 5;
-	private boolean fullGroup = false;
-	private int numCustomers;
-	private int nfs = 5;
-	private boolean first = true;
+	private volatile boolean fullGroup = false;
+	private volatile int nfs = 5;
+	private volatile boolean first = true;
 	
 	private ReentrantLock lock = new ReentrantLock();
 	Condition noGroup = lock.newCondition();
@@ -20,8 +18,8 @@ public class SushiMonitor_01 {
 		if(nfs == 0) {
 			fullGroup = true;
 		}
-		while (numCustomers == MAX_CAPACITY || fullGroup) {
-			System.out.println("----> Entering "+"C("+i+")");
+		System.out.println("----> Entering "+"C("+i+")");
+		while (nfs==0 || fullGroup) {
 			if (first) {
 				System.out.println(" *** Possible group detected. I wait "+"C("+i+")");
 				first = false;
@@ -32,7 +30,6 @@ public class SushiMonitor_01 {
 		}
 		
 		System.out.println(" +++ [free: " + nfs + "] I sit down C("+i+")");
-		numCustomers++;
 		nfs--;
 		lock.unlock();
 	}
@@ -40,7 +37,6 @@ public class SushiMonitor_01 {
 	public void exit(int i) {
 		/* COMPLETE */
 		lock.lock();
-		numCustomers--;
 		nfs++;
 		System.out.println("---> Now leaving [free: "+ nfs + "] "+"C("+i+")");
 
